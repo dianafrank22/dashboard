cc53cf22b62c97afd0cc4e01bfc96bb109382ac5
Diana Frank <diana.frank22@gmail.com>
2016-12-26 13:37:41 -0500

    adds browserify and adds google api promise

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // import keys from "../secret/keys.js"
 // need to make sure there is a delay on api calls,
 // if key was hit a few minutes ago, don't let them do it again ?
@@ -52,3 +53,13 @@ function getLatLong(){
 		})
 	})
 }
+},{"./secret/keys":2}],2:[function(require,module,exports){
+var keys = {
+	forecast: "49a6e10c13411ac75c7c0ace5f506ea1",
+	newyorktimes: "a772dc3238a3480591085ec85e9986b1",
+	google: "AIzaSyAdzbJmIaEY7zzxDMc1PAC31Paq6AEqQSE",
+	mta: "0d05ae482bda033ad7c31c8ded81a7f8"
+}
+
+module.exports = keys
+},{}]},{},[1]);
