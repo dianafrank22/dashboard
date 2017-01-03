ef64c4d45600ea8126b358b9a524ed880518f687
Diana Frank <diana.frank22@gmail.com>
2017-01-03 12:24:43 -0500

    retrieves, parses, and appends nytimes info from api request

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,15 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// import keys from "../secret/keys.js"
-// need to make sure there is a delay on api calls,
-// if key was hit a few minutes ago, don't let them do it again ?
-
 var keys = require('./secret/keys')
+
+window.addEventListener('keyup', getKey)
 
 
 function getKey(e){
 	var key = e.keyCode
 	switch(key){
-		// change train to lirr? 
 		case 84:
 			train();
 			break;
@@ -24,36 +21,93 @@ function getKey(e){
 			break;
 		case 78:
 			createButtons()
-		// change this to show buttons to select which type of news to get 
-			// news(national)
-		// change this for type of news? 
-		// get technology, upshot, politics, national
 			break;
 		default:
 			console.log('no api called')
 	}
 }
 
+
+
+// news functions
 function createButtons(){
- var titles = ["Tech", "Upshot", "Politics", "National"]
- for(let i =0; i < titles.length; i++){
- 	var btn = document.createElement("BUTTON");
- 	var title = document.createTextNode(titles[i]);
- 	btn.appendChild(title)
- 	btn.setAttribute("id", titles[i])
- 	btn.setAttribute("class", "newsBtn")
- 	document.getElementById('news').appendChild(btn)
- }
+	var news = document.getElementById('news')
+		console.log(news.childNodes.length)
+
+	if(news.childNodes.length > 5){
+		//buttons already created
+	}else{
+		 var titles = ["Technology", "Upshot", "Politics", "National"]
+		 for(let i =0; i < titles.length; i++){
+ 			var btn = document.createElement("BUTTON");
+		 	var title = document.createTextNode(titles[i]);
+		 	btn.appendChild(title)
+		 	btn.setAttribute("id", titles[i])
+		 	btn.setAttribute("class", "newsBtn")
+		 	news.appendChild(btn)
+ 		}
+ 		addClickToButton()
+	}
+
 }
 
-function checkButton(){
 
+function news(btn){
+	var key = keys.newyorktimes
+	var type = btn.target.id
+	return new Promise(function(resolve, reject){
+		fetch("https://api.nytimes.com/svc/topstories/v2/"+type+".json?api-key="+key,{
+			method:"GET"
+		}).then((response) => {
+			if(response.status >= 400){
+				response.json().then((body) => {
+					reject(new Error(body.error))
+				})
+			}
+			response.json().then((body) =>{
+				resolve(body)
+			})
+		})
+	})
 }
 
-function train(){
-	console.log('in train, hit 84')			
+function addClickToButton(){
+	var buttons = document.getElementsByClassName("newsBtn");
+	for(let i =0; i <buttons.length; i++){
+		buttons[i].addEventListener('click', callNews, false)
+	}	
 }
 
+
+function callNews(e){
+	news(e).then((data)=>{
+		var newsDiv = document.getElementById("news-results")
+		var newsArray = data.results
+		for(var i=0; i < newsArray.length; i++){
+			var h2 = document.createElement('H2');
+			var title = document.createTextNode(newsArray[i].title)
+			h2.appendChild(title)
+			var aTag = document.createElement('a');
+			aTag.setAttribute('href', newsArray[i].url)
+			aTag.appendChild(h2)
+			var h4 = document.createElement('H4')
+			var author = document.createTextNode(newsArray[i].byline)
+			h4.appendChild(author)
+			var p = document.createElement("P")
+			var summary = document.createTextNode(newsArray[i].abstract)
+			p.appendChild(summary)
+			newsDiv.appendChild(aTag)
+			newsDiv.appendChild(h4)
+			newsDiv.appendChild(p)
+		}
+		// news-results div class 
+		// have to loop through data.results
+		// get title, url, abstract (like summary), byline (author)
+		// parse data here, then send it to addText
+	})
+}
+
+// weather functions
 function getWeather(lat, long){
 	var key = keys.forecast
 	return new Promise(function(resolve, reject){
@@ -68,29 +122,6 @@ function getWeather(lat, long){
 			response.json().then((body)=>{
 				var weatherInfo = {current: body.currently, daily: body.daily}
 				resolve(weatherInfo)
-			})
-		})
-	})
-}
-
-function addText(divName, info){
-	var content = document.createTextNode(info);
-	divName.appendChild(content);
-}
-
-function news(type){
-	var key = keys.newyorktimes
-	return new Promise(function(resolve, reject){
-		fetch("https://api.nytimes.com/svc/topstories/v2/"+type+".json?api-key="+key,{
-			method:"GET"
-		}).then((response) => {
-			if(response.status >= 400){
-				response.json().then((body) => {
-					reject(new Error(body.error))
-				})
-			}
-			response.json().then((body) =>{
-				console.log(body)
 			})
 		})
 	})
@@ -117,8 +148,19 @@ function getLatLong(){
 	})
 }
 
+function train(){
+	console.log('in train, hit 84')			
+}
 
-window.addEventListener('keyup', getKey)
+
+
+function addText(divName, info){
+	var content = document.createTextNode(info);
+	divName.appendChild(content);
+}
+
+
+
 },{"./secret/keys":2}],2:[function(require,module,exports){
 var keys = {
 	forecast: "49a6e10c13411ac75c7c0ace5f506ea1",
