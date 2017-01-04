de19115d144deab724c6412e0b5027828583222d
Diana Frank <diana.frank22@gmail.com>
2017-01-04 18:18:21 -0500

    changes env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-console.log(process.env)
+// require('dotenv').config();
+// console.log(process.env)
 
 const express      = require('express');
 const logger       = require('morgan');
