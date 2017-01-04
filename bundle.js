24157162bac5f26cbf09381a14cd0529e30b77ce
Diana Frank <diana.frank22@gmail.com>
2017-01-04 14:52:54 -0500

    adds node-sass, and adds appropriate scripts

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/Users/dfrank/code/projects/dashboard/main.js' from '/Users/dfrank/code/projects/dashboard'");
--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const express      = require('express');
 const logger       = require('morgan');
 const path         = require('path');
 const bodyParser   = require('body-parser');
+// const sass         = require('node-sass');
 const app          = express();
 const port         = process.env.PORT || 3000;
 
+// app.use(sass.middleware({
+// 	src: __dirname + '/sass',
+// 	dest: __dirname + '/public',
+// 	debug: true,
+// 	outputStyle: 'compressed'
+// }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
