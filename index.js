f83f5f262b27890556e75b254aa3de05ceb84dae
Diana Frank <diana.frank22@gmail.com>
2017-01-14 23:45:24 -0500

    adds animated icons for weather

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const port         = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'icons')));
 
 app.use(bodyParser.json());
 
